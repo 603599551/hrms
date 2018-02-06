e7759428d32bf57dd25fc674bb1833b75f4e27b8
malaoshi <65242847@qq.com>
2018-02-06 15:37:58 +0800

    实现登录、获取当前登录用户信息、修改当前登录用户的密码等功能

---
+++ src/com/ss/controllers/LoginCtrl.java
@@ -0,0 +1,72 @@
+package com.ss.controllers;
+
+import com.alibaba.fastjson.JSONObject;
+import com.bean.UserBean;
+import com.jfinal.KEY;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
+import com.utils.RequestTool;
+import com.utils.UserSessionUtil;
+import org.apache.commons.lang.StringUtils;
+import utils.bean.JsonHashMap;
+
+public class LoginCtrl extends BaseCtrl{
+
+    public void index(){
+        JsonHashMap jhm=new JsonHashMap();
+        try{
+            JSONObject json=RequestTool.getJson(getRequest());
+            String username=json.getString("username");
+            String password=json.getString("password");
+
+            if(StringUtils.isEmpty(username)){
+                jhm.putCode(-1).putMessage("用户名不能为空！");
+                renderJson(jhm);
+                return;
+            }
+            if(StringUtils.isEmpty(password)){
+                jhm.putCode(-1).putMessage("密码不能为空！");
+                renderJson(jhm);
+                return;
+            }
+            Record r = Db.findFirst("select * from staff where username=? and password=?", username, password);
+            if (r != null) {
+                String status=r.get("status");
+                if("6".equals(status)){
+                    jhm.putCode(-1).putMessage("离职员工不能登录！");
+                    renderJson(jhm);
+                    return;
+                }
+                UserBean ub=new UserBean();
+                ub.setId(r.get("id"));
+                ub.setName(r.getStr("username"));
+                ub.setRealName(r.getStr("name" ));
+                ub.setDeptId(r.getStr("dept"));
+                ub.setDeptName(r.getStr("dept_name"));
+                Object job=r.get("job");
+                if(job==null)
+                    job="";
+                else
+                    job=job+"";
+                ub.setJobId((String)job);
+                ub.setJobName(r.getStr("job_name"));
+                setSessionAttr(KEY.SESSION_USER,ub);
+                setCookie("userId", r.get("id"), 60 * 60 * 24 * 3);
+
+                jhm.putCode(1);
+                jhm.put("userId",r.get("id"));
+                jhm.put("sessionId",getSession().getId());
+                jhm.putMessage("登录成功！");
+            } else {
+                jhm.putCode(-1);
+                jhm.putMessage("用户名或密码错误！");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            jhm.putCode(-1).putMessage(e.toString());
+        }
+        renderJson(jhm);
+    }
+
+
+}
