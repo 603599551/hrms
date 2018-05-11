ce20f3834f068ca22ab623264834d1ddc20b1943
mayumin <65242847@qq.com>
2018-05-11 10:04:13 +0800

    实现门店接收入库

--- src/com/store/order/controllers/StoreOrderReceiverCtrl.java
+++ src/com/store/order/controllers/StoreOrderReceiverCtrl.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Record;
 import com.ss.controllers.BaseCtrl;
+import com.store.order.services.StoreOrderReceiveSrv;
 import com.utils.RequestTool;
 import com.utils.SelectUtil;
 import com.utils.UserSessionUtil;
@@ -18,14 +19,16 @@ import java.util.List;
  * 接收门店订单
  */
 public class StoreOrderReceiverCtrl extends BaseCtrl {
+    static final String showDetailByIdSQL="SELECT a1.*,(case when b1.number is null then 0 end )as stock_num FROM ( SELECT a.id,a.code,( SELECT name FROM goods_attribute WHERE id=a.attribute_2) AS attribute_2_text,a.name,( SELECT name FROM goods_unit WHERE id=a.unit) AS unit_text,a.send_num AS order_material_num,b.send_num as receive_num,a.material_id,a.sort,a.store_order_id FROM store_order_material a,warehouse_out_order_material b,warehouse_out_order c WHERE a.store_order_id=c.store_order_id AND c.id=b.warehouse_out_order_id AND a.material_id=b.material_id) a1 LEFT JOIN store_stock b1 ON a1.material_id=b1.material_id where a1.store_order_id=? ORDER BY a1.sort  ";
     /**
      * 根据订单id查询详细信息
      */
     public void showDetailById() {
+
         String id=getPara("id");
         JsonHashMap jhm=new JsonHashMap();
         try{
-            List<Record> list=Db.find("select * from store_order_material where store_order_id=? order by sort",id);
+            List<Record> list=Db.find(showDetailByIdSQL,id);
             jhm.putCode(1).put("list",list);
         }catch (Exception e){
             e.printStackTrace();
@@ -45,16 +48,18 @@ public class StoreOrderReceiverCtrl extends BaseCtrl {
         String arriveDate=getPara("arriveDate");
         UserSessionUtil usu=new UserSessionUtil(getRequest());
         JsonHashMap jhm=new JsonHashMap();
+
+        String sql="select a.id as warehouse_out_order_id,b.id as store_order_id,a.order_number as warehouse_out_order_number,substr(a.out_time,1,16) as out_time,b.order_number as store_order_number,substr(b.create_time,1,16) as create_time_short,b.arrive_date from warehouse_out_order a left join store_order b on a.store_order_id=b.id ";
         try{
-            SelectUtil selectUtil=new SelectUtil("select *,substr(create_time,1,16) as create_time_short from store_order ");
-            selectUtil.like("and order_number like ?",SelectUtil.NONE,orderCode,SelectUtil.WILDCARD_ASTERISK);
-            selectUtil.addWhere("and want_date=? ",SelectUtil.NOT_NULL_AND_NOT_EMPTY_STRING,wantDate);
-            selectUtil.addWhere("and arrive_date=?",SelectUtil.NOT_NULL_AND_NOT_EMPTY_STRING,arriveDate);
-            selectUtil.addWhere("and status=?",40);
-            selectUtil.addWhere("and store_id=?",usu.getUserBean().getDeptId());
-            selectUtil.order("order by create_time");
-            String sql=selectUtil.toString();
-            List<Record> list=Db.find(sql,selectUtil.getParameters());
+            SelectUtil selectUtil=new SelectUtil(sql);
+//            selectUtil.like("and order_number like ?",SelectUtil.NONE,orderCode,SelectUtil.WILDCARD_ASTERISK);
+//            selectUtil.addWhere("and want_date=? ",SelectUtil.NOT_NULL_AND_NOT_EMPTY_STRING,wantDate);
+//            selectUtil.addWhere("and arrive_date=?",SelectUtil.NOT_NULL_AND_NOT_EMPTY_STRING,arriveDate);
+            selectUtil.addWhere("and b.status=?",40);
+            selectUtil.addWhere("and b.store_id=?",usu.getUserBean().getDeptId());
+            selectUtil.order("order by b.arrive_date");
+            String sqlExe=selectUtil.toString();
+            List<Record> list=Db.find(sqlExe,selectUtil.getParameters());
             jhm.putCode(1).put("list",list);
         }catch (Exception e){
             e.printStackTrace();
@@ -68,23 +73,14 @@ public class StoreOrderReceiverCtrl extends BaseCtrl {
      */
     public void accept(){
         JsonHashMap jhm=new JsonHashMap();
+        UserSessionUtil usu=new UserSessionUtil(getRequest());
         try {
             JSONObject jsonObject = RequestTool.getJson(getRequest());
-            String storeOrderId=jsonObject.getString("store_order_id");
+            String storeOrderId=jsonObject.getString("id");
             JSONArray jsonArray=jsonObject.getJSONArray("list");
 
-            List dataList=new ArrayList();
-            for(Object obj:jsonArray){
-                JSONObject json=(JSONObject)obj;
-                String id=json.getString("id");
-                int realSendNum=json.getInteger("real_send_num");
-                dataList.add(new Object[]{realSendNum,id});
-            }
-            int[] numArray=Db.batch("update store_order_material set real_send_num=? where id=?",null,100);
-            int sum=0;
-            for(int numTemp:numArray){
-                sum=sum+numTemp;
-            }
+            StoreOrderReceiveSrv service=enhance(StoreOrderReceiveSrv.class);
+            service.accept(storeOrderId,jsonArray,usu);
             jhm.putCode(1).putMessage("操作成功！");
         }catch (Exception e){
             e.printStackTrace();
--- src/com/store/order/services/StoreOrderReceiveSrv.java
+++ src/com/store/order/services/StoreOrderReceiveSrv.java
@@ -7,14 +7,29 @@ import com.jfinal.aop.Before;
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Record;
 import com.jfinal.plugin.activerecord.tx.Tx;
+import com.utils.UserSessionUtil;
+import easy.util.DateTool;
+import easy.util.NumberUtils;
+import easy.util.UUIDTool;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
+/**
+ * 门店接收入库
+ */
 public class StoreOrderReceiveSrv {
 
+    /**
+     * 门店接收入库
+     * @param storeOrderId
+     * @param jsonArray
+     * @param usu
+     * @throws Exception
+     */
     @Before(Tx.class)
-    public void accept(String storeOrderId, JSONArray array) throws Exception{
+    public void accept(String storeOrderId, JSONArray jsonArray, UserSessionUtil usu) throws Exception{
         /*
          * 更新门店订单状态
          */
@@ -26,36 +41,135 @@ public class StoreOrderReceiveSrv {
         /*
         保存门店订单接收的数量
          */
-//        List<Record> list=Db.find("select * from store_order_material where store_order_id=? order by sort",storeOrderId);
-//
-        List<Object[]> batchDataList=new ArrayList<>();
-//        for(Record r:list){
-//            String materialId=r.getStr("material_id");
-//            for(Object elemInArray:array){
-//                JSONObject jsonInArray=(JSONObject)elemInArray;
-//                String materialIdInElem=jsonInArray.getString("material_id");
-//                int receiveNum=jsonInArray.getInteger("receive_num");
-//                if(materialId.equals(materialIdInElem)){
-//                    batchDataList.add(new Object[]{receiveNum,materialId,storeOrderId});
-//                    break;
-//                }
-//            }
-//        }
-//        String sql="update store_order_material set receive_num=? where materialIdInElem=? and store_order_id=?";
-        for(Object elemInArray:array){
+        int jsonArraySize=jsonArray.size();
+        Object[][] dataArray=new Object[jsonArraySize][2];
+        int i=0;
+        for(Object elemInArray:jsonArray){
             JSONObject jsonInArray=(JSONObject)elemInArray;
             String idInElem=jsonInArray.getString("id");
-//                String materialIdInElem=jsonInArray.getString("material_id");
+            String materialIdInElem=jsonInArray.getString("material_id");
             int receiveNum=jsonInArray.getInteger("receive_num");
-            batchDataList.add(new Object[]{receiveNum,idInElem});
+            dataArray[i][0]=receiveNum;
+            dataArray[i][1]=idInElem;
+            i++;
         }
         String sql="update store_order_material set receive_num=? where id=?";
-        int[] numArray=Db.batch(sql,(Object[][])batchDataList.toArray(),100);
+        int[] numArray=Db.batch(sql,dataArray,100);
         int sum=0;
         for(int numTemp:numArray){
             sum=sum+numTemp;
         }
+        doUpdateStoreStock(storeOrderId,usu);
+    }
+    /*
+    保存门店订单接收的数量
+    如果订单中的原材料，在本店库存中有，就执行更新数量的操作
+    如果没有，就执行保存操作
+     */
+    private void doUpdateStoreStock(String storeOrderId,UserSessionUtil usu){
+        /*
+        更新门店库存
+         */
+        //门店订单原材料
+        List<Record> storeOrderMaterialList=Db.find("select * from store_order_material where store_order_id=?",storeOrderId);
+        //当前门店库存list
+        List<Record> storeStockList=Db.find("select * from store_stock where store_id=?",usu.getUserBean().getDeptId());
 
 
+        List<Object[]> updateDataList=new ArrayList();
+        List<Record> insertDataList=new ArrayList();
+        buildData(usu,storeOrderMaterialList,storeStockList,insertDataList,updateDataList);
+
+        if(updateDataList!=null && !updateDataList.isEmpty()){
+            Object[][] dataArray=new Object[updateDataList.size()][2];
+            int i=0;
+            for(Object[] arrayTemp:updateDataList){
+                dataArray[i]=arrayTemp;
+                i++;
+            }
+            int[] numArray=Db.batch("update store_stock set number=? where id=?",dataArray,100);
+            int numSum=0;
+            for(int numTemp:numArray){
+                numSum=numSum+numTemp;
+            }
+        }
+        if(insertDataList!=null && !insertDataList.isEmpty()){
+            int[] numArray=Db.batchSave("store_stock",insertDataList,100);
+            int numSum=0;
+            for(int numTemp:numArray){
+                numSum=numSum+numTemp;
+            }
+        }
+    }
+
+    /**
+     * 构建数据
+     * 如果订单中的原材料，在本店库存中有，就将该记录放入到updateDataList中
+     如果没有，就将记录放入到insertDataList中
+     * @param usu
+     * @param storeOrderMaterialList
+     * @param storeStockList
+     * @param insertDataList
+     * @param updateDataList
+     */
+    private void buildData(UserSessionUtil usu,List<Record> storeOrderMaterialList,List<Record> storeStockList,List<Record> insertDataList,List<Object[]> updateDataList){
+        String datetime= DateTool.GetDateTime();
+        String store_id=usu.getUserBean().getDeptId();
+        /*
+        查询当前门店的库存最大序号
+         */
+        int maxSort=getMaxSortOfStoreStock(usu.getUserBean().getDeptId());
+
+        for(Record storeOrderMaterialR:storeOrderMaterialList){
+            String materialIdOfStoreOrderMaterialR=storeOrderMaterialR.getStr("material_id");
+            String codeOfStoreOrderMaterialR=storeOrderMaterialR.getStr("code");
+            String nameOfStoreOrderMaterialR=storeOrderMaterialR.getStr("name");
+            String pinyinOfStoreOrderMaterialR=storeOrderMaterialR.getStr("pinyin");
+            String attribute1OfStoreOrderMaterialR=storeOrderMaterialR.getStr("attribute_1");
+            String attribute2OfStoreOrderMaterialR=storeOrderMaterialR.getStr("attribute_2");
+            String unitOfStoreOrderMaterialR=storeOrderMaterialR.getStr("unit");
+            int receiveNum=storeOrderMaterialR.getInt("receive_num");
+            for(Record storeStockR:storeStockList){
+                String idOfStoreStockR=storeStockR.getStr("id");
+                String materialIdOfStoreStockR=storeStockR.getStr("material_id");
+                String numberOfStoreStockR=storeStockR.getStr("number");
+                if(materialIdOfStoreOrderMaterialR.equals(materialIdOfStoreStockR)){//如果相同，就放入到updateDataList里，用于后续的更新
+                    updateDataList.add(new Object[]{receiveNum+numberOfStoreStockR,idOfStoreStockR});
+                    continue;
+                }
+            }
+
+            Record r=new Record();
+            r.set("id", UUIDTool.getUUID());
+            r.set("store_id", store_id);
+            r.set("material_id", materialIdOfStoreOrderMaterialR);
+            r.set("code", codeOfStoreOrderMaterialR);
+            r.set("name", nameOfStoreOrderMaterialR);
+            r.set("pinyin", pinyinOfStoreOrderMaterialR);
+            r.set("attribute_1", attribute1OfStoreOrderMaterialR);
+            r.set("attribute_2", attribute2OfStoreOrderMaterialR);
+            r.set("unit", unitOfStoreOrderMaterialR);
+            r.set("number", receiveNum);
+            r.set("modify_time", datetime);
+            r.set("sort", maxSort);
+            maxSort++;
+            insertDataList.add(r);
+        }
+    }
+
+    /**
+     * 根据store_id获取该门店库存的最大sort
+     * @param storeId
+     * @return
+     */
+    private int getMaxSortOfStoreStock(String storeId){
+        Record maxR=Db.findFirst("select max(sort) as max from store_stock where store_id=?",storeId);
+        int maxSort=1;
+        if(maxR!=null){
+            Object maxSortObj=maxR.get("max");
+            maxSort= NumberUtils.parseInt(maxSortObj,0);
+        }
+        if(maxSort==0)maxSort=1;
+        return maxSort;
     }
 }
