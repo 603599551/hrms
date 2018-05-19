f8c1b064cc330c17921e4da21b6e04365a7734e9
wangze <w@1.com>
2018-05-19 17:07:25 +0800

    修改Config

--- src/com/jfinal/Config.java
+++ src/com/jfinal/Config.java
@@ -10,6 +10,7 @@ import com.jfinal.plugin.ehcache.EhCachePlugin;
 import com.jfinal.render.ViewType;
 import com.jfinal.template.Engine;
 import com.logistics.order.controllers.OutWarehouseOrderCtrl;
+import com.logistics.order.controllers.StoreScrapCtrl;
 import com.ss.controllers.HomeCtrl;
 import com.ss.controllers.LoginCtrl;
 import com.ss.controllers.MenuCtrl;
@@ -89,6 +90,10 @@ public class Config extends JFinalConfig {
 		 */
 		//门店退货单
 		routes.add("/mgr/store/returnGoods", ReturnGoodsCtrl.class);
+		//门店废弃单
+		routes.add("/mgr/store/storeScrapManager", StoreScrapManagerCtrl.class);
+		//物流处理废弃单
+		routes.add("/mgr/store/storeScrap", StoreScrapCtrl.class);
 		//门店接收订单
 		routes.add("/mgr/store/storeOrderReceiver", StoreOrderReceiverCtrl.class);
 
