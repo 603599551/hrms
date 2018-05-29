cf77fc6c5651d3fc59c68cbccade9838e55c05dd
mayumin <65242847@qq.com>
2018-05-29 11:29:58 +0800

    门店id从用户信息中获取

--- src/com/store/controllers/StoreStockManagerCtrl.java
+++ src/com/store/controllers/StoreStockManagerCtrl.java
@@ -3,6 +3,7 @@ package com.store.controllers;
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Record;
 import com.ss.controllers.BaseCtrl;
+import com.utils.UserSessionUtil;
 import utils.bean.JsonHashMap;
 
 import java.util.List;
@@ -19,9 +20,10 @@ public class StoreStockManagerCtrl extends BaseCtrl{
      * 显示库存余额
      */
     public void showBalance(){
+        UserSessionUtil usu=new UserSessionUtil(getRequest());
         JsonHashMap jhm=new JsonHashMap();
-        String storeId=getPara("store_id");
-        String sql="select id,code,name,(select name from goods_attribute where goods_attribute.id=a.attribute_2) as attribute_2_text,(select name from goods_unit where goods_unit.id=a.unit) as unit_text ,number from store_stock a where store_id=? order by code,id";
+        String storeId=usu.getUserBean().getDeptId();
+        String sql="select id,code,name,(select name from goods_attribute where goods_attribute.id=a.attribute_2) as attribute_2_text,(select name from goods_unit where goods_unit.id=a.unit) as unit_text ,number from store_stock a where store_id=? order by type_1,type_2,code,id";
         List<Record> list=Db.find(sql,storeId);
         jhm.putCode(1).put("list",list);
         renderJson(jhm);
