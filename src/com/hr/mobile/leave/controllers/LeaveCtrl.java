47c87a1a7e9cc9f7c6b9bfa5794f57fb837934a8
mayumin <65242847@qq.com>
2018-07-28 11:17:28 +0800

    演示请假类

---
+++ src/com/hr/mobile/leave/controllers/LeaveCtrl.java
@@ -0,0 +1,6 @@
+package com.hr.mobile.leave.controllers;
+
+import com.common.controllers.BaseCtrl;
+
+public class LeaveCtrl extends BaseCtrl {
+}
---
+++ src/com/hr/mobile/leave/services/LeaveSrv.java
@@ -0,0 +1,4 @@
+package com.hr.mobile.leave.services;
+
+public class LeaveSrv {
+}
