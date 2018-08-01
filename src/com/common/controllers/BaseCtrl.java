78180bf96cc75932cd42bd376f822fe3d14a1b4e
wangze <w@1.com>
2018-08-01 09:32:46 +0800

    排班相关代码

--- src/com/common/controllers/BaseCtrl.java
+++ src/com/common/controllers/BaseCtrl.java
@@ -5,7 +5,9 @@ import com.jfinal.core.Controller;
 import com.jfinal.plugin.activerecord.Record;
 
 import java.io.BufferedReader;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,6 +15,7 @@ import java.util.Map;
 public abstract class BaseCtrl extends Controller {
 
     protected SimpleDateFormat sdf_ymd = new SimpleDateFormat("yyyy-MM-dd");
+    protected SimpleDateFormat sdf_year = new SimpleDateFormat("yyyy");
     protected static final long ONE_DAY_TIME = 1000 * 60 * 60 *24;
 
     /**
@@ -64,6 +67,19 @@ public abstract class BaseCtrl extends Controller {
         }
         return result;
     }
+
+    protected String nextDay(String date, int nextDay){
+        String result = "";
+        try {
+            Date today = sdf_ymd.parse(date);
+            today = new Date(today.getTime() + nextDay * ONE_DAY_TIME);
+            result = sdf_ymd.format(today);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
 
     public void add(){
 
