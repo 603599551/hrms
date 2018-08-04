85b9e337749baab85fa6daf74eac9276f21df624
Administrator <2601992846@qq.com>
2018-08-04 09:04:20 +0800

    app端请假功能，实现接口： 1.根据人员id、日期查看请假信息 2.店员请假 3店长查看请假列表 4.店长审核请假 5.店长查看请假信息

--- src/com/hr/mobile/leave/controllers/LeaveCtrl.java
+++ src/com/hr/mobile/leave/controllers/LeaveCtrl.java
@@ -11,11 +11,161 @@ import easy.util.UUIDTool;
 import org.apache.commons.lang.StringUtils;
 import utils.bean.JsonHashMap;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class LeaveCtrl extends BaseCtrl {
 
+
+    /**
+     * 5.1.根据人员id、日期查看请假信息
+     名称	根据人员id、日期查看请假信息
+     描述	根据人员id，日期查看请假信息
+     验证	根据人员id验证员工是否存在
+     日期格式是否正确
+     如果是店长，可查看本店所有店员请假信息
+     如果是店员，只能查看自己的请假信息
+     权限
+     URL	http://localhost:8081/mgr/mobile/leave/showDetailByStaffIdAndDate
+     请求方式	post
+     请求参数类型	key=value
+
+     请求参数列表：
+     参数名	类型	最大长度	允许空	描述
+     id	string		否	员工id
+     date	string		否	日期，格式：yyyy-MM-dd
+
+     返回数据：
+     返回格式	JSON
+     成功	{
+     "code": 1,
+     "staff_id": "10239723894",
+     "date": "2018-07-20",
+     "list":  [{
+     "start": "07:00",
+     "end": "07:15"
+     }, {
+     "start": "07:15",
+     "end": "07:30"
+     }]
+
+     }
+     staff_id：员工id
+     date：当天日期
+     list：请假时间段
+     失败	{
+     "code": 0,
+     "message": "员工不存在！"
+     }
+     或者
+     {
+     "code": 0,
+     "message": ""//失败信息
+     }
+     报错	{
+     "code": -1,
+     "message": "服务器发生异常！"
+     }
+     */
+
+    public void showDetailByStaffIdAndDate(){
+        JsonHashMap jhm = new JsonHashMap();
+        String id = getPara("id");
+        String date = getPara("date");
+
+        //进行非空判断
+        if(StringUtils.isEmpty(id)){
+            jhm.putCode(0).putMessage("id不能为空！");
+            renderJson(jhm);
+            return;
+        }
+        if(StringUtils.isEmpty(date)){
+            jhm.putCode(0).putMessage("日期不能为空！");
+            renderJson(jhm);
+            return;
+        }
+
+        try {
+            String sql = "select count(*) as c, s.job as job from h_staff s where id = ? ";
+            Record record = Db.findFirst(sql, id);
+            if(record.getInt("c") != 0){
+                if(StringUtils.equals("store_manager",record.getStr("job"))){ //店长
+                    String noticeSearch = "SELECT leave_start_time AS lt, leave_end_time AS et FROM h_staff_leave s, h_notice n WHERE s.staff_id = n.sender_id AND s.leave_info_id = n.fid AND n.receiver_id = ? AND s.date = ? ";
+                    List<Record> recordList = Db.find(noticeSearch, id, date);
+                    jhm.put("staff_id", id);
+                    jhm.put("date", date);
+                    jhm.put("list", recordList);
+                    jhm.putCode(1);
+                } else {  //员工
+                    String noticeSearch = "select leave_start_time as lt, leave_end_time as et from h_staff_leave where staff_id = ? and date = ?";
+                    List <Record> recordList = Db.find(noticeSearch, id, date);
+                    jhm.put("staff_id", id);
+                    jhm.put("date", date);
+                    jhm.put("list", recordList);
+                    jhm.putCode(1);
+                }
+            } else {
+                jhm.putCode(0).putMessage("员工不存在！");
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+            jhm.putCode(-1).putMessage("服务器发生异常！");
+        }
+        renderJson(jhm);
+    }
+
+
+
+
+    /**
+     * 5.2.店员请假
+     名称	店员请假
+     描述	店员请假。
+     将请假信息保存到h_staff_leave表，并给店长发通知，将请假通知保存h_notice表
+
+     验证	根据人员id验证员工是否存在
+     日期格式是否正确
+     权限	无
+     URL	http://localhost:8081/mgr/mobile/leave/apply
+     请求方式	post
+     请求参数类型	key=value
+
+     请求参数列表：
+     参数名	类型	最大长度	允许空	描述
+     id	string		否	员工id
+     date	string		否	日期，格式：yyyy-MM-dd
+     time	array		否	时间
+     [{
+     "start": "07:00",
+     "end": "07:15"
+     }, {
+     "start": "07:15",
+     "end": "07:30"
+     }]
+
+     reason	string	150	否	请假原因
+
+     返回数据：
+     返回格式	JSON
+     成功	{
+     "code": 1,
+     "message": "提交成功，等待店长审核！"
+     }
+
+     失败	{
+     "code": 0,
+     "message": "员工不存在！"
+     }
+     或者
+     {
+     "code": 0,
+     "message": ""//失败信息
+     }
+     报错	{
+     "code": -1,
+     "message": "服务器发生异常！"
+     }
+     */
     public void apply(){
         JsonHashMap jhm=new JsonHashMap();
         //获取用户信息的工具类
@@ -26,8 +176,16 @@ public class LeaveCtrl extends BaseCtrl {
          */
         String date=getPara("date");
         String time=getPara("time");
+        String reason = getPara("reason");
+
+        //进行非空判断
         if(StringUtils.isBlank(date)){
             jhm.putCode(0).putMessage("请输入请假的日期！");
+            renderJson(jhm);
+            return;
+        }
+        if(StringUtils.isBlank("reason")){
+            jhm.putCode(0).putMessage("请输入请假原因！");
             renderJson(jhm);
             return;
         }
@@ -35,6 +193,7 @@ public class LeaveCtrl extends BaseCtrl {
         paraMap.put("usu",usu);
         paraMap.put("date",date);
         paraMap.put("time",time);
+        paraMap.put("reason",reason);
 
         try {
             /*
@@ -47,11 +206,290 @@ public class LeaveCtrl extends BaseCtrl {
             e.printStackTrace();
             jhm.putCode(-1).putMessage("服务器发生错误！");
         }
-        System.out.println("11111111");
         renderJson(jhm);
     }
 
-    public void review(){
 
+    /**
+     * 5.3.店长查看请假列表
+     名称	请假列表
+     描述	查看请假列表信息，分页
+     未审核信息全部查询，已审核的信息，每次查询10条记录
+     验证	只有店长可使用
+     权限	店长
+     URL	http://localhost:8081/mgr/mobile/leave/list
+     请求方式	post
+     请求参数类型	key=value
+
+     请求参数列表：
+     参数名	类型	最大长度	允许空	描述
+     pageNum	int		是	页数
+     pageSize	int		是	每页显示记录数
+
+     返回数据：
+     返回格式	JSON
+     成功	{
+     "code": 1,
+     "reviewList": [{
+     "pic": "头像url",
+     "name": "李雷",
+     "job": "职位",
+     "date": "2018-07-30",
+     "day": "周五",
+     "time": "15:00-16:00",
+     "reason": "天气太热，要家里蹲"
+     }],
+     "reviewedList": [{
+     "pic": "头像url",
+     "name": "李雷",
+     "job": "职位",
+     "date": "2018-07-30",
+     "day": "周五",
+     "time": "15:00-16:00",
+     "reason": "天气太热，要家里蹲"
+     }]
+
+     }
+     reviewList：待审核
+     reviewedList：已审核
+     失败	{
+     "code": 0,
+     "message": "员工不存在！"
+     }
+     或者
+     {
+     "code": 0,
+     "message": ""//失败信息
+     }
+     报错	{
+     "code": -1,
+     "message": "服务器发生异常！"
+     }
+     */
+
+    public void list(){
+        JsonHashMap jhm = new JsonHashMap();
+        String pageNumStr = getPara("pageNum");
+        String pageSizeStr = getPara("pageSize");
+        UserSessionUtil usu = new UserSessionUtil(getRequest());
+
+
+        //为空时赋予默认值
+        int pageNum = NumberUtils.parseInt(pageNumStr, 1);
+        int pageSize = NumberUtils.parseInt(pageSizeStr, 10);
+
+        try {
+            //找出登陆人是哪个店
+            String id = usu.getUserId();
+            String deptSearch = "select s.dept_id as dept from h_staff s where id = ?";
+            Record  record = Db.findFirst(deptSearch, id);
+
+
+            String sql = "SELECT s.pic AS pic, s. NAME AS name, ( SELECT d.`name` FROM h_dictionary d WHERE d.`value` = s.job ) job, i.date AS date, i.times AS time, i.reason AS reason, i.id as leave_info_id FROM h_staff s, h_staff_leave_info i WHERE i.staff_id = s.id AND store_id = ? AND i.`status` = '0' ";
+            List<Record> recordList = Db.find(sql, record.getStr("dept"));
+
+            DateTool dateTool = new DateTool();
+            for(int i = 0; i < recordList.size(); i++){
+                String date = recordList.get(i).getStr("date");
+                dateTool = new DateTool();
+                String day = dateTool.getWeekDayName(date);
+                //获取星期几
+                recordList.get(i).set("day",day);
+            }
+            jhm.put("reviewList",recordList);
+
+            //已审核
+            String sqlReviewed = "SELECT s.pic AS pic, s. NAME AS name, ( SELECT d.`name` FROM h_dictionary d WHERE d.`value` = s.job ) job, i.`status` AS status, i.date AS date, i.times AS time, i.reason AS reason, i.id as leave_info_id  FROM h_staff s, h_staff_leave_info i WHERE i.staff_id = s.id AND store_id = ? AND ( i.`status` = '1' OR i.`status` = '2' )";
+            List<Record> records = Db.find(sqlReviewed, record.getStr("dept"));
+            for(int i = 0; i < records.size(); i++){
+                String date = records.get(i).getStr("date");
+                dateTool = new DateTool();
+                String day = dateTool.getWeekDayName(date);
+                records.get(i).set("day",day);
+                if(StringUtils.equals(records.get(i).getStr("status"),"1")){ //若数据库中status为1(同意)时返回1，若为2(拒绝)时返回0
+                    records.get(i).set("status","1");
+                } else {
+                    records.get(i).set("status","0");
+                }
+            }
+            jhm.put("reviewedList", records);
+
+            jhm.putCode(1);
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+            jhm.putCode(-1).putMessage("服务器发生异常！");
+        }
+        renderJson(jhm);
     }
+
+
+    /**
+     * 5.4.店长审核请假
+     名称	审核请假
+     描述	店长审核本店店员请假
+     更新h_staff_leave表，并给请假人发通知，将请假通知保存h_notice表
+     验证	根据id判断记录是否存在
+     是否店长
+     权限	店长
+     URL	http://localhost:8081/mgr/mobile/leave/review
+     请求方式	post
+     请求参数类型	key=value
+
+     请求参数列表：
+     参数名	类型	最大长度	允许空	描述
+     id	string		否	请假id
+     status	string		否	0:审核通过
+     1:审核拒绝
+     result	string	150	否	审核结果
+
+     返回数据：
+     返回格式	JSON
+     成功	{
+     "code": 1,
+     "message": "提交成功！"
+     }
+
+     失败	{
+     "code": 0,
+     "message": "记录不存在！"
+     }
+     或者
+     {
+     "code": 0,
+     "message": ""//失败信息
+     }
+     报错	{
+     "code": -1,
+     "message": "服务器发生异常！"
+     }
+     */
+
+    public void review(){
+        JsonHashMap jhm = new JsonHashMap();
+        String leaveId = getPara("id");
+        String status = getPara("status");
+        String result = getPara("result");
+        UserSessionUtil usu = new UserSessionUtil(getRequest());
+
+        //进行非空判断
+        if(StringUtils.isEmpty(leaveId)){
+            jhm.putCode(0).putMessage("请假id不能为空!");
+            renderJson(jhm);
+            return;
+        }
+        if(StringUtils.isEmpty(status)){
+            jhm.putCode(0).putMessage("请审核！");
+            renderJson(jhm);
+            return;
+        }
+        if(StringUtils.isEmpty(result) && StringUtils.equals("1",status)){
+            jhm.putCode(0).putMessage("审核结果不能为空！");
+            renderJson(jhm);
+            return;
+        }
+
+        Map paraMap=new HashMap();
+        paraMap.put("usu",usu);
+        paraMap.put("leaveId",leaveId);
+        paraMap.put("status",status);
+        paraMap.put("result",result);
+
+        try {
+            LeaveSrv srv = enhance(LeaveSrv.class);
+            jhm=srv.review(paraMap);
+        } catch (Exception e) {
+            e.printStackTrace();
+            jhm.putCode(-1).putMessage("服务器发生异常！");
+        }
+        renderJson(jhm);
+    }
+
+
+    /**
+     * 店长查看请假信息
+     名称	店长查看请假信息
+     描述	店长查看请假申请的详细信息
+     验证	根据id判断记录是否存在
+     [新增接口，店长查看员工请假信息]
+     权限	店长
+     URL	http://localhost:8081/mgr/mobile/leave/showById
+     请求方式	post
+     请求参数类型	key=value
+
+     请求参数列表：
+     参数名	类型	最大长度	允许空	描述
+     id	string		否	请假id
+
+
+
+     返回数据：
+     返回格式	JSON
+     成功	"code": 1,
+     {
+     "pic": "头像url",
+     "name": "李雷",
+     "job": "职位",
+     “phone”:”133xxxxxxxx”电话号码
+     "date": "2018-07-30",
+     "day": "周五",
+     "time": "15:00-16:00",
+     "reason": "天气太热，要家里蹲"
+     “status”:”0/1” 审核结果
+     “result”:””审核结果回复
+     },
+
+     失败	{
+     "code": 0,
+     "message": "记录不存在！"
+     }
+     或者
+     {
+     "code": 0,
+     "message": ""//失败信息
+     }
+     报错	{
+     "code": -1,
+     "message": "服务器发生异常！"
+     }
+     */
+
+    public void showById(){
+        JsonHashMap jhm = new JsonHashMap();
+        DateTool dateTool = new DateTool();
+        String leaveId = getPara("id");
+
+        //进行非空验证
+        if(StringUtils.isEmpty(leaveId)){
+            jhm.putCode(0).putMessage("请假id不能为空！");
+            renderJson(jhm);
+            return;
+        }
+
+        try {
+            String sql = "select count(*) as c from h_staff_leave_info where id = ? ";
+            Record countR = Db.findFirst(sql, leaveId);
+            if(countR.getInt("c") > 0){
+                String search = "SELECT s.pic as pic, s.name as name, (SELECT d.name from h_dictionary d where d.value = s.job)as job, s.phone as phone, i.date as date, i.times as time, i.reason as reason, i.status as status, i.result as result from h_staff s, h_staff_leave_info i where i.staff_id = s.id and i.id = ? ";
+                Record record = Db.findFirst(search, leaveId);
+                record.set("day", dateTool.getWeekDayName(record.getStr("date")));
+                //返回status,0为拒绝,1为同意
+                if(StringUtils.equals("1",record.getStr("status"))){
+                    record.set("status","1");
+                } else {
+                    record.set("status","0");
+                }
+                jhm.put("data",record);
+                jhm.putCode(1);
+            } else {
+                jhm.putCode(0).putMessage("记录不存在！");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            jhm.putCode(-1).putMessage("服务器发生异常！");
+        }
+        renderJson(jhm);
+    }
+
 }
--- src/com/hr/mobile/leave/services/LeaveSrv.java
+++ src/com/hr/mobile/leave/services/LeaveSrv.java
@@ -7,9 +7,14 @@ import com.jfinal.plugin.activerecord.tx.Tx;
 import com.utils.UserSessionUtil;
 import easy.util.DateTool;
 import easy.util.NumberUtils;
+import org.apache.commons.lang.StringUtils;
 import easy.util.UUIDTool;
+import net.sf.json.JSONArray;
+import sun.swing.StringUIClientPropertyKey;
 import utils.bean.JsonHashMap;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class LeaveSrv {
@@ -21,56 +26,155 @@ public class LeaveSrv {
         UserSessionUtil usu = (UserSessionUtil) paraMap.get("usu");
         String date = (String) paraMap.get("date");
         String time = (String) paraMap.get("time");
+        String reason = (String) paraMap.get("reason");
+        Record record = new Record(); //记录分表数据
+        List <Record> recordList = new ArrayList<>(); //整合分表数据
+        Record r = new Record();  //记录总表数据
 
         String userId = usu.getUserId();
 
         JsonHashMap jhm = new JsonHashMap();
 
-        String[] timeArray = time.split("-");
+        JSONArray jsonArray = new JSONArray();
+        jsonArray = JSONArray.fromObject(time);
         String datetime = DateTool.GetDateTime();//yyyy-MM-dd HH:mm:ss
-        String sql = "select count(*) as c from h_staff_leave where staff_id=? and store_id=? and date=? and leave_start_time=? and leave_end_time=? and status='0'";
-//        try {
-        Record countR = Db.findFirst(sql, userId, usu.getUserBean().getDeptId(), date, timeArray[0], timeArray[1]);
-        Object cObj = countR.get("c");
-        int c = NumberUtils.parseInt(cObj, 0);
-        if (c > 0) {
-            jhm.putCode(0).putMessage("请不要重复提交数据！");
-            return jhm;
+        try {
+            String leave_info_id = UUIDTool.getUUID();  //请假分表外键
+            String []timeArray = new String[2];
+            for(int i = 0; i < jsonArray.size(); i++){
+                record = new Record();
+                timeArray[0] = (String)jsonArray.getJSONObject(i).get("start");
+                timeArray[1] = (String)jsonArray.getJSONObject(i).get("end");
+                String sql = "select count(*) as c from h_staff_leave where staff_id=? and store_id=? and date=? and leave_start_time=? and leave_end_time=? and status='0'";
+                Record countR = Db.findFirst(sql, userId, usu.getUserBean().getDeptId(), date, timeArray[0], timeArray[1]);
+                Object cObj = countR.get("c");
+                int c = NumberUtils.parseInt(cObj, 0);
+                if (c > 0) {
+                    jhm.putCode(0).putMessage("请不要重复提交数据！");
+                    return jhm;
+                }
+                //请假分表操作
+                record.set("staff_id", userId);
+                record.set("date",date);
+                record.set("id", UUIDTool.getUUID());
+                record.set("leave_start_time", timeArray[0]);
+                record.set("leave_end_time", timeArray[1]);
+                record.set("leave_info_id",leave_info_id);
+                recordList.add(record);
+//                boolean flagL = Db.save("h_staff_leave", recordList.get(i));
+//                Db.batchSave("tablename", list, list.size());
+            }
+            int[] flagBatchSave = Db.batchSave("h_staff_leave", recordList, recordList.size());
+            //请假总表操作
+            r.set("id",leave_info_id);
+            r.set("staff_id", userId);
+            r.set("store_id", usu.getUserBean().getDeptId());
+            r.set("date", date);
+            r.set("status", "0");
+            r.set("reason",reason);
+            r.set("creater_id", userId);
+            r.set("create_time", datetime);
+            r.set("modifier_id", userId);
+            r.set("modify_time", datetime);
+            r.set("number",jsonArray.size());
+            //请求总表time字段
+//            String timeSearch = "select leave_start_time as lt, leave_end_time as et from h_staff_leave where leave_info_id = ? ";
+//            List <Record> timeList = Db.find(timeSearch, leave_info_id);
+            r.set("times",recordList.get(0).getStr("leave_start_time")+ "-" + recordList.get(jsonArray.size()-1).getStr("leave_end_time"));
+
+            boolean flagAll = Db.save("h_staff_leave_info", r);
+
+            //通知表操作
+            Record noticeR = new Record();
+            noticeR.set("id",UUIDTool.getUUID());
+
+            //获取员工名字添加到标题和内容中
+            Record nameR = Db.findById("h_staff", userId);
+            String name = nameR.getStr("name");
+
+            noticeR.set("title",name + "的请假申请");
+            noticeR.set("content",name + "的请假申请");
+            noticeR.set("sender_id", userId);
+
+            //获取该员工餐厅经理的id，job暂时不查字典值表，用store_manager代替
+            String managerSearch = "select s.id as id from h_staff s where dept_id = ? and job = 'store_manager' ";
+            Record managerR = Db.findFirst(managerSearch, usu.getUserBean().getDeptId());
+            String managerId = managerR.getStr("id");
+            noticeR.set("receiver_id", managerId);
+
+            noticeR.set("create_time", datetime);
+            noticeR.set("status","0");
+            noticeR.set("type", "leave");
+
+            //获取外键：请假表的该条数据id
+            noticeR.set("fid",r.getStr("id"));
+            boolean flagN = Db.save("h_notice", noticeR);
+
+            if(flagN && flagAll){
+                jhm.putCode(1).putMessage("提交成功，请等待审核！");
+            } else {
+                jhm.putCode(0).putMessage("提交失败！");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            jhm.putCode(-1).putMessage("服务器发生异常！");
+            throw e;
         }
-
-        Record r = new Record();
-        r.set("id", UUIDTool.getUUID());
-        r.set("staff_id", userId);
-        r.set("store_id", usu.getUserBean().getDeptId());
-        r.set("date", date);
-        r.set("leave_start_time", timeArray[0]);
-        r.set("leave_end_time", timeArray[1]);
-        r.set("status", 0);
-        r.set("creater_id", userId);
-        r.set("create_time", datetime);
-        r.set("modifier_id", userId);
-        r.set("modify_time", datetime);
-
-        Record noticeR = new Record();
-        noticeR.set("id", "1");
-
-        boolean flag = Db.save("h_staff_leave", r);
-        Db.save("h_notice", noticeR);
+        return jhm;
+    }
 
 
-        if (flag) {
-            jhm.putCode(1).putMessage("提交成功！");
-        } else {
-            jhm.putCode(0).putMessage("提交失败！");
+    /*
+    增加事务
+     */
+    @Before(Tx.class)
+    public JsonHashMap review(Map paraMap) {
+        JsonHashMap jhm = new JsonHashMap();
+
+        UserSessionUtil usu = (UserSessionUtil) paraMap.get("usu");
+        String leaveId = (String) paraMap.get("leaveId");
+        String status = (String) paraMap.get("status");
+        String result = (String) paraMap.get("result");
+        String dateTime = DateTool.GetDateTime();
+
+        try {
+            String sql = " select count(*) as c from h_staff_leave_info i where i.id = ? ";
+            Record countL = Db.findFirst(sql, leaveId);
+            if(countL.getInt("c") > 0){
+                Record leaveRecord = Db.findById("h_staff_leave_info", leaveId);
+                if(StringUtils.equals("0",status)){
+                    leaveRecord.set("status", "1");
+                } else {
+                    leaveRecord.set("status","2");
+                }
+                leaveRecord.set("result", result);
+                boolean flag = Db.update("h_staff_leave_info", leaveRecord);
+                if(flag){
+                    String noticeSearch = "select n.title, n.content, n.sender_id, n.receiver_id, n.type, n.fid from h_notice n where fid = ? ";
+                    Record record = Db.findFirst(noticeSearch, leaveRecord.getStr("id"));
+                    record.set("id", UUIDTool.getUUID());
+                    String temp = record.getStr("sender_id");
+                    record.set("sender_id", record.getStr("receiver_id"));
+                    record.set("receiver_id", temp);
+                    record.set("create_time", dateTime);
+                    record.set("status","0");
+                    boolean flagN = Db.save("h_notice", record);
+                    if(flagN){
+                        jhm.putCode(1).putMessage("审核完成！");
+                    } else {
+                        jhm.putCode(0).putMessage("审核失败！");
+                    }
+                } else {
+                    jhm.putCode(0).putMessage("审核失败！");
+                }
+            } else {
+                jhm.putCode(0).putMessage("记录不存在！");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            jhm.putCode(-1).putMessage("服务器发生异常！");
         }
-//        }catch (Exception e){
-//            jhm.putCode(-1).putMessage("服务器发生错误！");
-//            /*
-//            必须抛出异常，否则事务不启用
-//             */
-//            throw e;
-//        }
-
         return jhm;
     }
 }
