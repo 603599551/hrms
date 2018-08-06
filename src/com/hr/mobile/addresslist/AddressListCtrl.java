576538389380e8767804c7dfd05c2e137de11ef5
zhanjinqi <603599551@qq.com>
2018-08-06 18:12:37 +0800

    实习客户端通知接口

--- src/com/hr/mobile/addresslist/AddressListCtrl.java
+++ src/com/hr/mobile/addresslist/AddressListCtrl.java
@@ -3,6 +3,7 @@ package com.hr.mobile.addresslist;
 import com.common.controllers.BaseCtrl;
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Record;
+import easy.util.NumberUtils;
 import utils.bean.JsonHashMap;
 
 import java.util.ArrayList;
@@ -88,23 +89,42 @@ public class AddressListCtrl extends BaseCtrl {
         JsonHashMap jhm = new JsonHashMap();
         String deptId = getPara("deptid");
         String job = getPara("job");  //职位
-        String kind = getPara("kind"); //岗位
+        String kind = getPara("kind"); //岗位 得到一个
         String init;    //首字母
         try {
-            String sql;
+            String sql="";
+            String sql7="";
+            String sql6="";
+            String jobNames="";
             List<Record> list;
+            List<Record> list7;
             //若全部职位参与排序
             if (job.equals("all")) {
-                sql = "select name,pinyin,job,phone from h_staff where dept_id=? order by left(pinyin,1) ASC";
+                sql = "select id,name,pinyin,job,phone,kind from h_staff where dept_id=? order by left(pinyin,1) ASC";
                 list = Db.find(sql, deptId);
                 if (list != null && list.size() > 0) {
-
                     for (Record r : list) {
                         init=r.getStr("pinyin").substring(0, 1);
                         r.set("pinyin",init);
                         //若是员工，则显示岗位
                         if (r.getStr("job") .equals("staff")) {
                             r.set("job", r.getStr("kind"));
+                            sql7="select h_dictionary.name as name from h_dictionary,h_staff where find_in_set(h_dictionary.value,h_staff.kind) and h_staff.id=?";
+                            list7=Db.find(sql7,r.getStr("id"));
+                            if (list7 != null && list7.size() > 0){
+                                for (Record r7:list7){
+                                    if (r7==list7.get(0)){
+                                        jobNames=r7.getStr("name");
+                                    }else{
+                                        jobNames+=","+r7.getStr("name");
+                                    }
+                                }
+                            }
+                            r.set("job",jobNames);
+                        }else{
+                            sql6="select h_dictionary.name as name from h_dictionary,h_staff where h_staff.job=h_dictionary.value and h_staff.id=?";
+                            String jobName=Db.findFirst(sql6,r.getStr("id")).getStr("name");
+                            r.set("job",jobName);
                         }
                     }
                 }
@@ -113,12 +133,35 @@ public class AddressListCtrl extends BaseCtrl {
             else {
                 //员工职位 返回岗位
                 if (job.equals("staff")) {
-                    sql = "select name,pinyin,kind,phone from h_staff where dept_id=? and kind=? order by left(pinyin,1) ASC";
+                    sql = "select id,name,pinyin,kind,phone from h_staff where dept_id=? and kind like CONCAT('%',?,'%') order by left(pinyin,1) ASC";
                     list = Db.find(sql, deptId, kind);
+                    if (list != null && list.size() > 0){
+                        for (Record r : list){
+                            sql7="select h_dictionary.name as name from h_dictionary,h_staff where find_in_set(h_dictionary.value,h_staff.kind) and h_staff.id=?";
+                            list7=Db.find(sql7,r.getStr("id"));
+                            if (list7 != null && list7.size() > 0){
+                                for (Record r7:list7){
+                                    if (r7==list7.get(0)){
+                                        jobNames=r7.getStr("name");
+                                    }else{
+                                        jobNames+=","+r7.getStr("name");
+                                    }
+                                }
+                            }
+                            r.set("kind",jobNames);
+                        }
+                    }
                 }//非员工 则返回职位
                 else {
-                    sql = "select name,pinyin,job,phone from h_staff where dept_id=? and job=? order by left(pinyin,1) ASC";
+                    sql = "select id,name,pinyin,job,phone from h_staff where dept_id=? and job=? order by left(pinyin,1) ASC";
                     list = Db.find(sql, deptId, job);
+                    if (list != null && list.size() > 0){
+                        for (Record r : list){
+                            sql6="select h_dictionary.name as name from h_dictionary,h_staff where h_staff.job=h_dictionary.value and h_staff.id=?";
+                            String jobName=Db.findFirst(sql6,r.getStr("id")).getStr("name");
+                            r.set("job",jobName);
+                        }
+                    }
                 }
                 if (list != null && list.size() > 0) {
                     for (Record r : list) {
@@ -139,25 +182,25 @@ public class AddressListCtrl extends BaseCtrl {
 
             //遍历list将数据按首字母分组
             if (list != null && list.size() > 0) {
-                for (Record r : list) {
-                    count++;
-                    if(initial.equals(r.getStr("pinyin"))){
-                        l.add(r);
-                    }else{
-                        //首次
-                        if(initial.equals("")){
-                            l.add(r);
-                        }
-                        else{
+                for (int i=0;i<list.size();i++){
+                    if (i==0){
+                        initial=list.get(0).getStr("pinyin");
+                        l.add(list.get(0));
+                    }else {
+                        if (initial.equals(list.get(i).getStr("pinyin"))){
+                            l.add(list.get(i));
+                        }else {
                             sList.add(l);
                             l=new ArrayList<>();
-                            l.add(r);
-                            if(r==list.get(list.size()-1)){
-                                sList.add(l);
-                            }
+                            initial=list.get(i).getStr("pinyin");
+                            l.add(list.get(i));
                         }
+
                     }
-                    initial=r.getStr("pinyin");
+                    if (i==list.size()-1){
+                        sList.add(l);
+                    }
+                    count++;
                 }
             }
             jhm.put("data", sList);
@@ -231,13 +274,18 @@ public class AddressListCtrl extends BaseCtrl {
             String sql3;    //查找每个人的迟到次数
             String sql4;    //查找每个人的早退次数
             String staffId;//人员id
+            String sql7="";
+            String sql6="";
+            String jobNames="";
+            List<Record> list7;
             List<Record> list;
             sql2="select count(*) from h_staff_leave_info where store_id=? and staff_id=? and date>=? and date<=? and status='1'";
             sql3="select count(*) from h_staff_clock where store_id=? and staff_id=? and date>=? and date<=? and is_late=2";
             sql4="select count(*) from h_staff_clock where store_id=? and staff_id=? and date>=? and date<=? and is_leave_early=2";
+
             //若全部职位参与排序
             if (job.equals("all")) {
-                sql = "select id,name,pinyin,job,phone from h_staff where dept_id=? order by left(pinyin,1) ASC";
+                sql = "select id,name,pinyin,job,phone,kind from h_staff where dept_id=? order by left(pinyin,1) ASC";
                 list = Db.find(sql, deptId);
                 if (list != null && list.size() > 0) {
                     for (Record r : list) {
@@ -251,8 +299,24 @@ public class AddressListCtrl extends BaseCtrl {
                         init=r.getStr("pinyin").substring(0, 1);
                         r.set("pinyin",init);
                         //若是员工，则显示岗位
-                        if (r.getStr("job") .equals("staff") ) {
+                        if (r.getStr("job") .equals("staff")) {
                             r.set("job", r.getStr("kind"));
+                            sql7="select h_dictionary.name as name from h_dictionary,h_staff where find_in_set(h_dictionary.value,h_staff.kind) and h_staff.id=?";
+                            list7=Db.find(sql7,r.getStr("id"));
+                            if (list7 != null && list7.size() > 0){
+                                for (Record r7:list7){
+                                    if (r7==list7.get(0)){
+                                        jobNames=r7.getStr("name");
+                                    }else{
+                                        jobNames+=","+r7.getStr("name");
+                                    }
+                                }
+                            }
+                            r.set("job",jobNames);
+                        }else{
+                            sql6="select h_dictionary.name as name from h_dictionary,h_staff where h_staff.job=h_dictionary.value and h_staff.id=?";
+                            String jobName=Db.findFirst(sql6,r.getStr("id")).getStr("name");
+                            r.set("job",jobName);
                         }
                     }
                 }
@@ -261,12 +325,35 @@ public class AddressListCtrl extends BaseCtrl {
             else {
                 //员工职位 返回岗位
                 if (job.equals("staff")) {
-                    sql = "select id,name,pinyin,kind,phone from h_staff where dept_id=? and kind=? order by left(pinyin,1) ASC";
+                    sql = "select id,name,pinyin,kind,phone from h_staff where dept_id=? and kind like CONCAT('%',?,'%') order by left(pinyin,1) ASC";
                     list = Db.find(sql, deptId, kind);
+                    if (list != null && list.size() > 0){
+                        for (Record r : list){
+                            sql7="select h_dictionary.name as name from h_dictionary,h_staff where find_in_set(h_dictionary.value,h_staff.kind) and h_staff.id=?";
+                            list7=Db.find(sql7,r.getStr("id"));
+                            if (list7 != null && list7.size() > 0){
+                                for (Record r7:list7){
+                                    if (r7==list7.get(0)){
+                                        jobNames=r7.getStr("name");
+                                    }else{
+                                        jobNames+=","+r7.getStr("name");
+                                    }
+                                }
+                            }
+                            r.set("kind",jobNames);
+                        }
+                    }
                 }//非员工 则返回职位
                 else {
                     sql = "select id,name,pinyin,job,phone from h_staff where dept_id=? and job=? order by left(pinyin,1) ASC";
                     list = Db.find(sql, deptId, job);
+                    if (list != null && list.size() > 0){
+                        for (Record r : list){
+                            sql6="select h_dictionary.name as name from h_dictionary,h_staff where h_staff.job=h_dictionary.value and h_staff.id=?";
+                            String jobName=Db.findFirst(sql6,r.getStr("id")).getStr("name");
+                            r.set("job",jobName);
+                        }
+                    }
                 }
                 if (list != null && list.size() > 0) {
                     for (Record r : list) {
@@ -295,25 +382,25 @@ public class AddressListCtrl extends BaseCtrl {
 
             //遍历list将数据按首字母分组
             if (list != null && list.size() > 0) {
-                for (Record r : list) {
-                    count++;
-                    if(initial.equals(r.getStr("pinyin"))){
-                        l.add(r);
-                    }else{
-                        //首次
-                        if(initial.equals("")){
-                            l.add(r);
-                        }
-                        else{
+                for (int i=0;i<list.size();i++){
+                    if (i==0){
+                        initial=list.get(0).getStr("pinyin");
+                        l.add(list.get(0));
+                    }else {
+                        if (initial.equals(list.get(i).getStr("pinyin"))){
+                            l.add(list.get(i));
+                        }else {
                             sList.add(l);
                             l=new ArrayList<>();
-                            l.add(r);
-                            if(r==list.get(list.size()-1)){
-                                sList.add(l);
-                            }
+                            initial=list.get(i).getStr("pinyin");
+                            l.add(list.get(i));
                         }
+
                     }
-                    initial=r.getStr("pinyin");
+                    if (i==list.size()-1){
+                        sList.add(l);
+                    }
+                    count++;
                 }
             }
             jhm.put("data", sList);
@@ -325,4 +412,74 @@ public class AddressListCtrl extends BaseCtrl {
         renderJson(jhm);
     }
 
+    public void addressNum(){
+        JsonHashMap jhm = new JsonHashMap();
+        String deptId = getPara("deptid");
+        String job1="store_manager";
+        String job2="assistant_manager";
+        String job3="trainee_manager";
+        String job4="trainer";
+        String kind1="waiter";
+        String kind2="passed";
+        String kind3="band";
+        String kind4="cleanup";
+        String kind5="inputorder";
+        String kind6="cashier";
+
+        try{
+            String sql="select count(*)as c from h_staff where kind like CONCAT('%',?,'%') and dept_id=?";
+            String sql2="select count(*)as c from h_staff where job=? and dept_id=?";
+            //数据类型有可能是int long 等等
+            Object countObj1=Db.findFirst(sql2,job1,deptId).get("c");
+            //将object转化为int
+            int count1= NumberUtils.parseInt(countObj1,0);
+
+            Object countObj2=Db.findFirst(sql2,job2,deptId).get("c");
+            int count2= NumberUtils.parseInt(countObj2,0);
+
+            Object countObj3=Db.findFirst(sql2,job3,deptId).get("c");
+            int count3= NumberUtils.parseInt(countObj3,0);
+
+            Object countObj4=Db.findFirst(sql2,job4,deptId).get("c");
+            int count4= NumberUtils.parseInt(countObj4,0);
+
+            Object countObj5=Db.findFirst(sql,kind1,deptId).get("c");
+            int count5= NumberUtils.parseInt(countObj5,0);
+
+            Object countObj6=Db.findFirst(sql,kind2,deptId).get("c");
+            int count6= NumberUtils.parseInt(countObj6,0);
+
+            Object countObj7=Db.findFirst(sql,kind3,deptId).get("c");
+            int count7= NumberUtils.parseInt(countObj7,0);
+
+            Object countObj8=Db.findFirst(sql,kind4,deptId).get("c");
+            int count8= NumberUtils.parseInt(countObj8,0);
+
+            Object countObj9=Db.findFirst(sql,kind5,deptId).get("c");
+            int count9= NumberUtils.parseInt(countObj9,0);
+
+            Object countObj10=Db.findFirst(sql,kind6,deptId).get("c");
+            int count10= NumberUtils.parseInt(countObj10,0);
+
+            Record r=new Record();
+            r.set("store_managerNum",count1);
+            r.set("assistant_managerNum",count2);
+            r.set("trainee_managerNum",count3);
+            r.set("trainerNum",count4);
+            r.set("waiterNum",count5);
+            r.set("passedNum",count6);
+            r.set("bandNum",count7);
+            r.set("cleanupNum",count8);
+            r.set("inputorderNum",count9);
+            r.set("cashierNum",count10);
+
+            jhm.putCode(1);
+            jhm.put("numbers",r);
+
+        }catch (Exception e){
+            e.printStackTrace();
+            jhm.putCode(-1).putMessage("服务器发生异常！");
+        }
+        renderJson(jhm);
+    }
 }
