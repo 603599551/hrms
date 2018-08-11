1ce761849a0284a2970fc28a8b859a59ed1e0305
zww <3494443252@qq.com>
2018-08-11 14:37:20 +0800

    录入员工_自动生成工号

---
+++ src/com/common/service/OrderNumberGenerator.java
@@ -0,0 +1,61 @@
+package com.common.service;
+
+import com.common.controllers.BaseCtrl;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
+import easy.util.DateTool;
+
+/**
+ * 生成订单编号的公共类
+ */
+public class OrderNumberGenerator extends BaseCtrl {
+    /**
+     * 补零长度
+     */
+    static final int LENGTH=4;
+
+    /**
+     * 员工工号
+     */
+    static final String TYPE_GH="GH";
+
+    /**
+     * 生成员工工号
+     * @return
+     */
+    public static synchronized String getOutWarehouseOrderNumber(){
+        String reStr="";
+        String date= DateTool.GetDate();
+
+        try {
+            Record r=Db.findFirst("select * from order_number where type=?",TYPE_GH);
+            if(r!=null){//如果有记录就继续判断
+                String dateInR=r.getStr("date");
+                if(date.equals(dateInR)){//如果日期相同
+                    int number=r.getInt("number");
+                    number++;
+                    reStr=getNumber(TYPE_GH,date,LENGTH,number);
+                    Db.update("update order_number set number=? where type=?",number,TYPE_GH);
+                }else{//如果数据库中的日期不是当前系统日期
+                    Db.update("update order_number set date=?,number=? where type=?",date,1,TYPE_GH);
+                    reStr=getNumber(TYPE_GH,date,LENGTH,1);
+                }
+            }else{//如果没有记录就添加记录
+                Record saveR=new Record();
+                saveR.set("date",date);
+                saveR.set("type",TYPE_GH);
+                saveR.set("number",1);
+                saveR.set("remark","员工工号");
+                Db.save("order_number",saveR);
+                reStr=getNumber(TYPE_GH,date,LENGTH,1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return reStr.replace("-","");
+    }
+    private static String getNumber(String type,String date,int length,int number){
+        return type+date+String.format("%0"+LENGTH+"d", number);
+    }
+
+}
